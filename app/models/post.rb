1820599c71b3b93c141e4c5e3055c85b389e7474
koki kanasaka <k.koki980701@gmail.com>
2021-07-05 15:53:22 +0900

    contentのバリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :content, presence: true, length: { minimum: 1, maximum: 1000 }
 end
